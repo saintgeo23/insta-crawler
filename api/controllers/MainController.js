e4fc51d943b8f0e9a529f35de169d69f7f7564b0
George Volokitin <saintgeo23@gmail.com>
2017-06-20 01:53:10 +0300

    Sails app initialized

---
+++ api/controllers/MainController.js
@@ -0,0 +1,7 @@
+const nunjucks = require('nunjucks');
+
+module.exports = {
+  index(req, res) {
+    return res.view();
+  },
+};
---
+++ config/views.js
@@ -0,0 +1,7 @@
+const nunjucks = require('nunjucks');
+
+module.exports.views = {
+  engine: 'nunjucks',
+  layout: false,
+  partials: false,
+};
